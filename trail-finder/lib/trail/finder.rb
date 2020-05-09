ffb9921565256e28ee5f395202ee51ab57ecd0e7
Seth Todd <seth.todd@outlook.com>
2020-05-09 18:09:38 +0000

    Automatically backed up by Learn

--- trail-finder/lib/trail/finder.rb
+++
@@ -1,8 +0,0 @@
-require "trail/finder/version"
-
-module Trail
-  module Finder
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- trail-finder/lib/trail/finder/version.rb
+++
@@ -1,5 +0,0 @@
-module Trail
-  module Finder
-    VERSION = "0.1.0"
-  end
-end
